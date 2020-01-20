3bf73e680b742842e2fd445f271a26df3ef59cfc
aldrian96 <mhmdsaldrian96@gmail.com>
2020-01-20 23:19:45 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title', 'MOCHIIZOU')
+
+@section('container')
+<div class="container">
+      <div class="row">
+        <div class="col-10">
+          <h1 class="mt-3">Hello, Aldrian!</h1>
+        </div>
+      </div>  
+    </div>
+@endsection
+
